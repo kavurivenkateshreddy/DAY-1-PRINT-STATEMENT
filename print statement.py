378ccaf9585026cd54cafd721f7c30b6b1a6d129
Venkatesh Reddy Kavuri <104256379+kavurivenkateshreddy@users.noreply.github.com>
2022-06-16 19:40:54 +0530

    Update print statement.py

--- print statement.py
+++ print statement.py
@@ -3,28 +3,6 @@ Date: 18th April 2022 Monday
 Name: kavuri venkatesh reddy
 File Desc: print statement
 """
-'''print> to print something or display some
-message on the screen'''
-print('python') #python
-print("python") #python
-print('python"fullstack') #python"fullstack
-print("python'fullstack") #python'fullstack
-print('python\'fullstack') #python'fullstack
-print("python\"fullstack") #python"fullstack
-print('hi \
-i am \
-venkatesh') #hi i am venkatesh
-print('''hi i am venkatesh''') #hi i am venkatesh
-print("""hi i am venkatesh""") #hi i am Venkatesh
-print('''hi i' am" venkatesh''' ) #hi i' am" venkatesh
-print("""hi i' am" venkatesh""" ) #hi i' am" venkatesh
-print('''hi i\''' am""" venkatesh''') #hi i''' am""" venkatesh
-print("""hi i\""" am''' venkatesh""") #hi i""" am''' venkatesh
-"""
-Date: 18th April 2022 Monday
-Name: kavuri venkatesh reddy
-File Desc: print statement
-"""
 """
 INTRODUCTION TO PYTHON 
 
