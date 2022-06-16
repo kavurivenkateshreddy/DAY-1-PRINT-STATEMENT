5da4f29d0b02962474d9246af90f11538fe3abae
Venkatesh Reddy Kavuri <104256379+kavurivenkateshreddy@users.noreply.github.com>
2022-06-16 19:40:10 +0530

    Update print statement.py

--- print statement.py
+++ print statement.py
@@ -20,3 +20,69 @@ print('''hi i' am" venkatesh''' ) #hi i' am" venkatesh
 print("""hi i' am" venkatesh""" ) #hi i' am" venkatesh
 print('''hi i\''' am""" venkatesh''') #hi i''' am""" venkatesh
 print("""hi i\""" am''' venkatesh""") #hi i""" am''' venkatesh
+"""
+Date: 18th April 2022 Monday
+Name: kavuri venkatesh reddy
+File Desc: print statement
+"""
+"""
+INTRODUCTION TO PYTHON 
+
+--->Python is interpreter.
+
+--->interpreter means it executes the code line by line.
+Ex: Python
+
+--->compiler: it executes the all at one time.
+Ex: C
+
+print introduction
+
+Every print statement takes a new line.
+
+
+
+COMMENT IN PYTHON:-
+what is the use of comment?
+--->Comments helps us to understand the line of code or program.
+Note: Comments will never get executed in the program.
+
+we have two types of comments.
+1) Single line comment:-
+
+ It is represented by #  ( Hash , Compound Sign)
+
+The shortcut for keeping or removing the single line comment is ctrl+/
+
+2) Multiline comment
+
+--->It is represented by three times single quotes or three timesdouble quotes.
+Ex:  """
+"""
+Name:
+Date & Day: 
+Time:
+File Description:
+"""
+'''
+INTRODUCTION TO PRINT STATEMENT :
+--->print> to print something or display some message on the screen
+--->The print() function prints the specified message to the screen, or other standard output device.
+--->The message can be a string, or any other object, the object will be converted into a string before written to the screen
+--->Every print statement takes a new line.
+'''
+print('python') #python
+print("python") #python
+print('python"fullstack') #python"fullstack
+print("python'fullstack") #python'fullstack
+print('python\'fullstack') #python'fullstack
+print("python\"fullstack") #python"fullstack
+print('hi \
+i am \
+venkatesh') #hi i am venkatesh
+print('''hi i am venkatesh''') #hi i am venkatesh
+print("""hi i am venkatesh""") #hi i am Venkatesh
+print('''hi i' am" venkatesh''' ) #hi i' am" venkatesh
+print("""hi i' am" venkatesh""" ) #hi i' am" venkatesh
+print('''hi i\''' am""" venkatesh''') #hi i''' am""" venkatesh
+print("""hi i\""" am''' venkatesh""") #hi i""" am''' venkatesh
